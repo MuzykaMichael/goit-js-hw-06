99f70244e1d10bd8dec2a6aa777aa3dd57cbc80b
Muzyka Mykhailo <Muzyka_Mishel@ukr.net>
2023-04-30 12:45:47 +0300

    Update task-10.js

--- js/task-10.js
+++ js/task-10.js
@@ -3,33 +3,44 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
-const base = 30;
-const controls = document.querySelector('#controls')
 
 
-const input = document.querySelector('number');
-const boxes = document.querySelector('#boxes');
+const contr = {
+  input: document.querySelector("#controls input"),
+  createBtn: document.querySelector("button[data-create"),
+  destroyBtn: document.querySelector("button[data-destroy"),
+  boxes: document.querySelector("#boxes")
+};
 
-const boxHold = [];
+contr.createBtn.addEventListener("click", createBtnClick);
+contr.destroyBtn.addEventListener("click", destroyBtnClick);
 
+function createBtnClick() {
+  let boxHold = [];
+  const boxNumber = +contr.input.value;
+  const minValue = +contr.input.min;
+  const maxValue = +contr.input.max;
+  const step = +contr.input.step;
 
-function createBoxes(amount) {
-  if (amount > input.getAttribute("max") || amount < input.getAttribute("min")) {
-    alert("Ваше число не підходить! Оберіть інше.")
-  } else {
-    for (let i = 0; i <= amount; i+=input.getAttribute("step")) {
-      const newBox = boxes.createElement("div");
-      const grhc = getRandomHexColor();
-      newBox.style.backgroundColor = grhc;
-      newBox.style.width = `${base+i*10}px`;
-      newBox.style.height = `${base+i*10}px`;
-      boxHold.push(newBox);
-    }
-  boxes.append(...boxHold)
+  if (boxNumber < minValue || boxNumber > maxValue){
+    return
   }
+  for (let i = 0; i<boxNumber; i+=step) {
+    boxHold.push(i);
+  }
+
+  const markup = boxHold
+  .map((box,index) => {
+    box = `<div style="width: ${30 + index * 10}px; height: ${30 + index * 10}px; background-color: ${getRandomHexColor()};"></div>`;
+    return box;
+  })
+  .join("");
+
+  contr.boxes.insertAdjacentHTML("beforeend", markup);
+  boxHold = [];
 }
 
-function destroyBoxes () {
-  
+function destroyBtnClick() {
+  contr.boxes.innerHTML = "";
+  contr.input.value = "";
 }
-
